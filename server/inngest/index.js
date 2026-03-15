98544610899cedc1129eeeb3496ea0cb8a9f7c39
Saurabh Kumar <saurabhsin141@gmail.com>
2026-03-16 00:53:33 +0530

    email reminder added

--- server/inngest/index.js
+++ server/inngest/index.js
@@ -1,5 +1,7 @@
 import { Inngest } from "inngest";
 import User from "../models/User.js";
+import Connection from "../models/Connection.js";
+import sendEmail from "../configs/nodeMailer.js";
 
 // Create a client to send and receive events
 export const inngest = new Inngest({ id: "pulseup-app" });
@@ -45,11 +47,9 @@ const syncUserUpdation = inngest.createFunction(
       profile_picture: image_url,
     };
 
-    await User.findByIdAndUpdate(id, updatedUserData)
-
+    await User.findByIdAndUpdate(id, updatedUserData);
   },
 );
-
 
 //inngest function to delete user data from database
 const syncUserDeletion = inngest.createFunction(
@@ -58,10 +58,73 @@ const syncUserDeletion = inngest.createFunction(
   async ({ event }) => {
     const { id } = event.data;
 
-    await User.findByIdAndDelete(id)
+    await User.findByIdAndDelete(id);
+  },
+);
 
+//Inngest function to send reminder when a new connection request is added
+const sendNewConnectionRequestReminder = inngest.createFunction(
+  { id: "send-new-connection-request-reminder" },
+  { event: "app/connection-request" },
+  async ({ event, step }) => {
+    const { connectionId } = event.data;
+
+    await step.run("send-connection-request-mail", async () => {
+      const connection = await Connection.findById(connectionId).populate(
+        "from_user_id to_user_id",
+      );
+      const subject = `&#128075; New Connection Request`;
+      const body = `
+     <div style="font-family: Arial,sans-serif; padding:20px">
+      <h2>Hi ${connection.to_user_id.full_name},</h2>
+      <p>You have a new connection request from ${connection.from_user_id.full_name} - @${connection.from_user_id.username}</p>
+      <p>Click <a href="${process.env.FRONTEND_URL}/connections" style="color:#10b981;">here</a> to accept or reject the request</p>
+      <br />
+      <p>Thanks,<br />PulseUp - Stay Connected</p>
+
+    </div>
+     `;
+
+      await sendEmail({
+        to: connection.to_user_id.email,
+        subject,
+        body,
+      });
+    });
+
+    const in24Hours = new Date(Date.now() + 24 * 60 * 60 * 1000);
+    await step.sleepUntil("wait-for-24-hours", in24Hours);
+    await step.run("send-connection-request-reminder", async () => {
+      const connection = await Connection.findById(connectionId).populate(
+        "from_user_id to_user_id",
+      );
+
+      if (connection.status === "accepted") {
+        return { message: "Already accepted" };
+      }
+
+      const subject = `&#128075; New Connection Request`;
+      const body = `
+     <div style="font-family: Arial,sans-serif; padding:20px">
+      <h2>Hi ${connection.to_user_id.full_name},</h2>
+      <p>You have a new connection request from ${connection.from_user_id.full_name} - @${connection.from_user_id.username}</p>
+      <p>Click <a href="${process.env.FRONTEND_URL}/connections" style="color:#10b981;">here</a> to accept or reject the request</p>
+      <br />
+      <p>Thanks,<br />PulseUp - Stay Connected</p>
+
+    </div>
+     `;
+
+      await sendEmail({
+        to: connection.to_user_id.email,
+        subject,
+        body,
+      });
+
+      return {message: "Reminder sent."}
+    });
   },
 );
 
 // Create an empty array where we'll export future Inngest functions
-export const functions = [syncUserCreation, syncUserUpdation, syncUserDeletion];
+export const functions = [syncUserCreation, syncUserUpdation, syncUserDeletion, sendNewConnectionRequestReminder];
--- server/server.js
+++ server/server.js
@@ -4,6 +4,8 @@ import  'dotenv/config'
 import connectDB from './configs/db.js';
 import { functions, inngest } from './inngest/index.js'
 import { serve } from "inngest/express";
+import { clerkMiddleware } from '@clerk/express'
+import userRouter from './routes/userRotes.js';
 
 const app = express();
 
@@ -11,12 +13,13 @@ await connectDB();
 
 app.use(express.json())
 app.use(cors())
+app.use(clerkMiddleware())
 
 app.get('/', (req,res)=>{
     res.send("server is running")
 })
 app.use('/api/inngest', serve({client: inngest, functions}))
-
+app.use('/api/user', userRouter)
 
 const PORT = process.env.PORT  || 4000;
 app.listen(PORT, ()=>{
