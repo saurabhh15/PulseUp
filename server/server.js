a3c74eff54f452133c572eb5c43f0f324bba3e06
Saurabh Kumar <saurabhsin141@gmail.com>
2026-03-16 22:49:24 +0530

    add routes

--- server/server.js
+++ server/server.js
@@ -5,9 +5,9 @@ import connectDB from './configs/db.js';
 import { functions, inngest } from './inngest/index.js'
 import { serve } from "inngest/express";
 import { clerkMiddleware } from '@clerk/express'
-// import userRouter from './routes/userRoutes.js';
-// import postRouter from './routes/postRoutes.js';
-// import storyRouter from './routes/storyRoutes.js';
+import userRouter from './routes/userRoutes.js';
+import postRouter from './routes/postRoutes.js';
+import storyRouter from './routes/storyRoutes.js';
 // import messageRouter from './routes/messageRoutes.js';
 
 const app = express();
@@ -23,9 +23,9 @@ app.get('/', (req,res)=>{
 })
 app.use('/api/inngest', serve({client: inngest, functions}))
 
-// app.use('/api/user', userRouter)
-// app.use('/api/post', postRouter)
-// app.use('/api/story', storyRouter)
+app.use('/api/user', userRouter)
+app.use('/api/post', postRouter)
+app.use('/api/story', storyRouter)
 // app.use('/api/message', messageRouter)
 
 const PORT = process.env.PORT  || 4000;
